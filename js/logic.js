3a71f9f51afc6a28801d5d93a7f7724994cc63ad
MAMV3x3 <mamv3x3@gmail.com>
2022-09-07 17:26:03 -0500

    Pet picker logic added

---
+++ js/logic.js
@@ -0,0 +1,25 @@
+function startGame(){
+    let buttonPet = document.getElementById('button-pet')
+    buttonPet.addEventListener('click', pickPlayerPet)
+}
+
+function pickPlayerPet(){
+    let inputHipodoge = document.getElementById("hipodoge").checked
+    let inputCapipepo = document.getElementById("capipepo").checked
+    let inputRatigueya = document.getElementById("ratigueya").checked
+    let petPicked = ""
+    if(inputHipodoge){
+        petPicked = "Hipodoge"
+    } else if(inputCapipepo){
+        petPicked = "Capipepo"
+    } else if(inputRatigueya){
+        petPicked = "Ratigueya"
+    }
+    if(!petPicked){
+        alert("⚠️ Por favor, selecciona a tu mascota ⚠️")
+    } else{
+        alert("Haz seleccionado a " + petPicked)
+    }
+}
+
+window.addEventListener("load", startGame)
